f26544bbdb9cfdfa316a1b741f5e76789edf95bf
funkycowboy <agatlin3@hotmail.com>
2017-01-20 09:48:43 -0500

    fix issue with collspase menu and search slider not working

--- Angular/app/Layouts/public-layout.component.js
+++ Angular/app/Layouts/public-layout.component.js
@@ -12,6 +12,53 @@ var core_1 = require('@angular/core');
 var PublicLayoutComponent = (function () {
     function PublicLayoutComponent() {
     }
+    PublicLayoutComponent.prototype.ngOnInit = function () {
+        $('#ProductSearchButton').click(function () {
+            var $this = $(".sidenav");
+            if ($this.hasClass("on")) {
+                $this.removeClass("on").addClass("off");
+            }
+            else {
+                $this.removeClass("off").addClass("on");
+            }
+        });
+        $('#SlideIndicatorButton').click(function () {
+            var $this = $(".sidenav");
+            if ($this.hasClass("on")) {
+                $this.removeClass("on").addClass("off");
+                $this.removeClass("glyphicon-triangle-right").addClass("glyphicon-triangle-left");
+            }
+            else {
+                $this.removeClass("off").addClass("on");
+                $this.removeClass("glyphicon-triangle-left").addClass("glyphicon-triangle-right");
+            }
+        });
+        function lockScroll() {
+            $(document).bind("touchstart", function (event) {
+                event.preventDefault();
+            });
+            $(document).bind("touchmove", function (event) {
+                event.preventDefault();
+            });
+        }
+        function unLockScroll() {
+            $(document).unbind("touchstart");
+            $(document).unbind("touchmove");
+        }
+        function changeOrientation(event) {
+            $("#landscape").show();
+            $("#rotateplease").show();
+            if ($(window).width() > $(window).height()) {
+                lockScroll();
+            }
+            else {
+                unLockScroll();
+            }
+        }
+        $(window).on("orientationchange", function () {
+            changeOrientation(event);
+        });
+    };
     PublicLayoutComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
--- Angular/app/Layouts/public-layout.component.ts
+++ Angular/app/Layouts/public-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
     moduleId: module.id,
@@ -6,7 +8,61 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: 'public-layout.component.html',
     styleUrls: ['public-layout.component.css']
 })
-export class PublicLayoutComponent {
+export class PublicLayoutComponent implements OnInit {
 
+    ngOnInit(): void {
+        $('#ProductSearchButton').click(function () {
+                var $this = $(".sidenav");
+                if ($this.hasClass("on")) {
+                    $this.removeClass("on").addClass("off");
+                } else {
+                    $this.removeClass("off").addClass("on");
+                }
+        });
+
+        $('#SlideIndicatorButton').click(function () {
+            var $this = $(".sidenav");
+            if ($this.hasClass("on")) {
+                $this.removeClass("on").addClass("off");
+                $this.removeClass("glyphicon-triangle-right").addClass("glyphicon-triangle-left");
+
+            } else {
+                $this.removeClass("off").addClass("on");
+                $this.removeClass("glyphicon-triangle-left").addClass("glyphicon-triangle-right");
+            }
+
+        });
+
+        function lockScroll() {
+                $(document).bind("touchstart", function (event: Event) {
+                    event.preventDefault();
+                });
+                $(document).bind("touchmove", function (event: Event) {
+                    event.preventDefault();
+                });
+            }
+
+            function unLockScroll() {
+                $(document).unbind("touchstart");
+                $(document).unbind("touchmove");
+
+            }
+
+            function changeOrientation(event: Event) {
+                $("#landscape").show();
+                $("#rotateplease").show();
+            
+                if ($(window).width() > $(window).height()) {                
+                    lockScroll();
+                }
+                else {
+                    unLockScroll();
+                }
+            }
+
+            $(window).on("orientationchange", function () {
+                changeOrientation(event);
+            });
+    }
 
 }
